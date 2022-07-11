a1072b5fe5be38035ef244203c630e85c1c2b1cb
birdsong5 <108277183+birdsong5@users.noreply.github.com>
2022-07-11 17:18:21 +0200

    Added API to forecast

--- script.js
+++ script.js
@@ -41,33 +41,8 @@ if (minutesNow < 10) {
 let timeNow = document.querySelector(".time-now");
 timeNow.innerHTML = `${hourNow}:${minutesNow}`;
 
-function showWeather(response) {
-  console.log(response);
-  document.querySelector("h1").innerHTML = response.data.name;
-  let iconElement = document.querySelector("#icon");
-  celsiusTemperature = response.data.main.temp;
-  document.querySelector(".temperature-now-span").innerHTML =
-    Math.round(celsiusTemperature);
-  document.querySelector("h4").innerHTML = response.data.weather[0].description;
-  document.querySelector(
-    "#humidity"
-  ).innerHTML = `${response.data.main.humidity}%`;
-  document.querySelector("#wind").innerHTML = Math.round(
-    response.data.wind.speed
-  );
-  celsiusTemperatureFeelsLike = response.data.main.feels_like;
-
-  document.querySelector("#feels-like").innerHTML = Math.round(
-    celsiusTemperatureFeelsLike
-  );
-  iconElement.setAttribute(
-    "src",
-    `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
-  );
-  iconElement.setAttribute("alt", response.data.weather[0].description);
-}
-
-function showForecast() {
+function showForecast(response) {
+  console.log(response.data.daily);
   let forecastElement = document.querySelector("#forecast");
 
   let forecastHTML = `<div class="row">`;
@@ -97,6 +72,39 @@ function showForecast() {
 
   forecastHTML = forecastHTML + `</div>`;
   forecastElement.innerHTML = forecastHTML;
+}
+
+function getForecast(coordinates) {
+  let apiKey = "468253ff0d2c7b9f49a43bcd7fd91cbf";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=metric`;
+
+  axios.get(apiUrl).then(showForecast);
+}
+
+function showWeather(response) {
+  document.querySelector("h1").innerHTML = response.data.name;
+  let iconElement = document.querySelector("#icon");
+  celsiusTemperature = response.data.main.temp;
+  document.querySelector(".temperature-now-span").innerHTML =
+    Math.round(celsiusTemperature);
+  document.querySelector("h4").innerHTML = response.data.weather[0].description;
+  document.querySelector(
+    "#humidity"
+  ).innerHTML = `${response.data.main.humidity}%`;
+  document.querySelector("#wind").innerHTML = Math.round(
+    response.data.wind.speed
+  );
+  celsiusTemperatureFeelsLike = response.data.main.feels_like;
+
+  document.querySelector("#feels-like").innerHTML = Math.round(
+    celsiusTemperatureFeelsLike
+  );
+  iconElement.setAttribute(
+    "src",
+    `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
+  );
+  iconElement.setAttribute("alt", response.data.weather[0].description);
+  getForecast(response.data.coord);
 }
 
 function searchCityDefault(city) {
@@ -169,5 +177,4 @@ fahrenheitLink.addEventListener("click", displayFahrenheitUnits);
 let celsiusLink = document.querySelector("#celsius-link");
 celsiusLink.addEventListener("click", displayCelsiusUnits);
 
-showForecast();
 searchCityDefault("Prague");
